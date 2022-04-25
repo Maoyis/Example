ec9d13e7e36cfdd3db81b7a09b804bd4ce0bcfe8
QiaoXun <20605873+Maoyis@users.noreply.github.com>
2022-04-25 13:41:12 +0800

    mew

--- Example/download/DownloadPage.swift
+++ Example/download/DownloadPage.swift
@@ -17,6 +17,8 @@ class DownloadPage: page {
     lazy var table: UITableView = {
         let table = UITableView.init(frame: .zero, style: .insetGrouped)
         table.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        
+        table.register(DownloadTaskCell.self, forCellReuseIdentifier: DownloadTaskCell.description())
         table.tableFooterView = UIView()
         view.addSubview(table)
         table.dataSource = self
@@ -27,6 +29,7 @@ class DownloadPage: page {
     let data:[Item] = [
         .common
     ]
+    var tasks:[LXDownloadTask] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,25 +43,50 @@ class DownloadPage: page {
 
 
 extension DownloadPage : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        if section == 0 {
+            return data.count
+        }
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.description(), for: indexPath)
-        cell.accessoryType = .disclosureIndicator
-        cell.textLabel?.text = data[indexPath.item].rawValue
-        return cell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.description(), for: indexPath)
+            cell.accessoryType = .disclosureIndicator
+            cell.textLabel?.text = data[indexPath.item].rawValue
+            return cell
+        }else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: DownloadTaskCell.description(), for: indexPath) as! DownloadTaskCell
+            let task = tasks[indexPath.item]
+            if let commonTask = task as? LXCommonDownloadTask {
+                commonTask.delegate = cell
+            }
+            cell.task = task
+            return cell
+        }
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let item = data[indexPath.section]
-//        let link = "https://new.iskcd.com/20220408/1M9NVcM7/index.m3u8"
-        let link = "https://devstreaming-cdn.apple.com/videos/wwdc/2021/10143/8/02A20AB5-0C7F-4E9F-B252-75A25D1261ED/downloads/wwdc2021-10143_sd.mp4?dl=1"
-        guard let url = URL(string: link) else { return }
-        if item == .common {
-            let task = LXCommonDownloadTask.task(with: url)
-            LXDowanloader.default.download(with: task)
+        if indexPath.section == 0 {
+            let item = data[indexPath.section]
+    //        let link = "https://new.iskcd.com/20220408/1M9NVcM7/index.m3u8"
+            let link = "https://devstreaming-cdn.apple.com/videos/wwdc/2021/10143/8/02A20AB5-0C7F-4E9F-B252-75A25D1261ED/downloads/wwdc2021-10143_sd.mp4?dl=1&time=\(Date.now.timeIntervalSince1970)"
+            guard let url = URL(string: link) else { return }
+            if item == .common {
+                let task = LXCommonDownloadTask.task(with: url)
+                tasks.append(task)
+                LXDowanloader.default.download(with: task)
+                self.table.reloadData()
+            }
+        }else {
+            
         }
+        
     }
     
     
---
+++ Example/download/view/DownloadTaskCell.swift
@@ -0,0 +1,51 @@
+//
+//  DownloadTaskCell.swift
+//  Example
+//
+//  Created by admin on 4/25/22.
+//
+
+import UIKit
+import LXDowanloader
+
+class DownloadTaskCell: UITableViewCell {
+
+    var task:LXDownloadTask! {
+        didSet {
+            self.textLabel?.text = task.name
+            self.detailTextLabel?.text = "\(task.progress)"
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+extension DownloadTaskCell : LXDownloadTaskDelegate {
+    func update(progress: Float) {
+        OperationQueue.main.addOperation {[weak self] in
+            self?.detailTextLabel?.text = "\(progress)"
+        }
+        
+    }
+    
+    
+}
