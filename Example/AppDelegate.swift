1df2e2734fb887f4a2671631456e2d82bcaf4270
QiaoXun <20605873+Maoyis@users.noreply.github.com>
2022-04-25 18:32:50 +0800

    new

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -10,6 +10,7 @@ import CoreData
 
 
 import LXLayoutKit
+import LXDowanloader
 
 
 @main
@@ -18,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
         return true
     }
 
--- Example/download/DownloadPage.swift
+++ Example/download/DownloadPage.swift
@@ -14,6 +14,7 @@ class DownloadPage: page {
         case common
         case m3u8
     }
+    var downloader:LXDowanloader = LXDowanloader.default
     lazy var table: UITableView = {
         let table = UITableView.init(frame: .zero, style: .insetGrouped)
         table.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
@@ -27,7 +28,7 @@ class DownloadPage: page {
     }()
     
     let data:[Item] = [
-        .common
+        .common, .m3u8
     ]
     var tasks:[LXDownloadTask] = []
     
@@ -35,6 +36,11 @@ class DownloadPage: page {
         super.viewDidLoad()
         self.title = "下载"
         table.lx_tiling()
+        guard let cache = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else {
+            fatalError("no cache floder")
+        }
+        var floder = URL(fileURLWithPath: cache)
+        print(floder)
     }
 
 
@@ -73,18 +79,27 @@ extension DownloadPage : UITableViewDelegate, UITableViewDataSource {
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 0 {
-            let item = data[indexPath.section]
-    //        let link = "https://new.iskcd.com/20220408/1M9NVcM7/index.m3u8"
-            let link = "https://devstreaming-cdn.apple.com/videos/wwdc/2021/10143/8/02A20AB5-0C7F-4E9F-B252-75A25D1261ED/downloads/wwdc2021-10143_sd.mp4?dl=1&time=\(Date.now.timeIntervalSince1970)"
-            guard let url = URL(string: link) else { return }
+            let item = data[indexPath.item]
+            var task:LXDownloadTask!
             if item == .common {
-                let task = LXCommonDownloadTask.task(with: url)
-                tasks.append(task)
-                try! LXDowanloader.default.download(with: task)
-                self.table.reloadData()
+                let link = "https://devstreaming-cdn.apple.com/videos/wwdc/2021/10143/8/02A20AB5-0C7F-4E9F-B252-75A25D1261ED/downloads/wwdc2021-10143_sd.mp4?dl=1&time=\(Date.now.timeIntervalSince1970)"
+                guard let url = URL(string: link) else { return }
+                task = LXCommonDownloadTask.task(with: url, name: "task\(tasks.count)")
+            }else {
+                let link = "https://new.iskcd.com/20220408/1M9NVcM7/index.m3u8?time=\(Date.now.timeIntervalSince1970)"
+                guard let url = URL(string: link) else { return }
+                task = LXM3U8DownloadTask.task(with: url, name: "task\(tasks.count)")
             }
+            tasks.append(task)
+            try! downloader.download(with: task)
+            self.table.reloadData()
         }else {
-            
+            let task = tasks[indexPath.item]
+            if task.status != .suspend {
+                downloader.suspend(with: task)
+            }else {
+                try! downloader.download(with: task)
+            }
         }
         
     }
