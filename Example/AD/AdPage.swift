ebf68dae1291d1cfb8e179aaec63bf1fc3c8b9f2
QiaoXun <20605873+Maoyis@users.noreply.github.com>
2022-05-11 18:12:15 +0800

    】

--- Example/AD/AdPage.swift
+++ Example/AD/AdPage.swift
@@ -6,13 +6,42 @@
 //
 
 import UIKit
+import LXADKit
 
 class AdPage: page {
-
+    enum Item : String {
+        case launch = "开屏广告"
+        case banner = "横幅"
+        case interstitial = "插页广告"
+        case interstitialVideo = "插页视频"
+        case rewarded = "激励"
+        case rewardedInterstitial = "插页激励"
+        case native = "原生"
+        case nativeVideo = "原生视频"
+    }
+    lazy var table: UITableView = {
+        let table = UITableView.init(frame: .zero, style: .insetGrouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        
+        table.register(DownloadTaskCell.self, forCellReuseIdentifier: DownloadTaskCell.description())
+        table.tableFooterView = UIView()
+        view.addSubview(table)
+        table.dataSource = self
+        table.delegate   = self
+        return table
+    }()
+    var items:[Item] = [
+        .launch,
+        .interstitial, .interstitialVideo,
+        .rewarded, .rewardedInterstitial,
+        .native, .nativeVideo
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "广告"
-        // Do any additional setup after loading the view.
+        table.lx_tiling()
     }
     
 
@@ -26,4 +55,43 @@ class AdPage: page {
     }
     */
 
+}
+
+
+extension AdPage : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.description(), for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = items[indexPath.item].rawValue
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = items[indexPath.item]
+        switch item {
+        case .launch: AdMannager.default.launch()
+        case .interstitial: AdMannager.default.interstitial()
+        case .rewarded: AdMannager.default.rewarded()
+        case .native: AdMannager.default.native()
+
+        default:
+            break
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
 }
--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         LXDownloader.default.maxCount = 3
-        AdMannager.default.launch()
+        AdMannager.default.start()
 
         return true
     }
